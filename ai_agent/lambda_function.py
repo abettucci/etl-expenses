fd96e9da56101689021351a5bacbef145e5eaeb5
abettucci <abettucci@users.noreply.github.com>
2025-07-22 02:03:32 -0300

    fix ai_agent

--- ai_agent/lambda_function.py
+++ ai_agent/lambda_function.py
@@ -6,7 +6,7 @@ import requests
 import openai
 
 # Configuración inicial
-TELEGRAM_BOT_TOKEN = "REDACTED_TELEGRAM_BOT_TOKEN"
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 redshift_data = boto3.client('redshift-data')
