ffa179271762c3c45f59eaeb6787c1aa43ab6f02
abettucci <abettucci@users.noreply.github.com>
2025-08-25 12:14:11 -0300

    fix en transferencia de df a bq

--- redshift_to_bq/lambda_function.py
+++ redshift_to_bq/lambda_function.py
@@ -3,11 +3,10 @@ import pandas as pd
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
 from google.api_core.exceptions import GoogleAPICallError
-# from pandas_gbq import to_gbq
 import io
 import time
 import json
-import warnings
+from datetime import datetime
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 
@@ -46,11 +45,10 @@ def convert_column_types(df, table_name):
     basado en el nombre de la tabla y los nombres de las columnas.
     """
     if table_name == 'mp_data':
-        # Mapeo de tipos para la tabla mp_data
         type_mapping = {
             'source_id': 'string',
             'report_id': 'string',
-            'report_date': 'date',
+            'report_date': 'date',            # solo fecha
             'settlement_date': 'datetime64[ns]',
             'payment_method_type': 'string',
             'transaction_type': 'string',
@@ -65,12 +63,11 @@ def convert_column_types(df, table_name):
             'sub_unit': 'string'
         }
     elif table_name == 'bank_payments':
-        # Mapeo de tipos para la tabla bank_payments
         type_mapping = {
             'comercio' : 'string',
             'cuotas': 'int64',
             'extraido_en': 'datetime64[ns]',
-            'fecha_pago': 'date',
+            'fecha_pago': 'date',            # solo fecha
             'hora_pago': 'string',
             'id' : 'string',
             'message_id' : 'string',
@@ -78,11 +75,10 @@ def convert_column_types(df, table_name):
             'nro_tarjeta' : 'string',
             'tarjeta' : 'string',
         }
-    else: # market-tickets
-        # Mapeo de tipos para la tabla carrefour_data
+    else: # carrefour_data
         type_mapping = {
             'nro_ticket': 'int64',
-            'fecha': 'date',
+            'fecha': 'date',                # solo fecha
             'categ': 'string',
             'prod': 'string',
             'cant': 'int64',
@@ -94,29 +90,28 @@ def convert_column_types(df, table_name):
         }
     
     for col in df.columns:
-        # Si tenemos un mapeo específico, lo usamos
         if col in type_mapping:
             target_type = type_mapping[col]
             try:
-                if target_type.startswith('datetime'):
+                if target_type == 'date':
+                    # convertir a datetime y luego quedarnos solo con la fecha
+                    df[col] = pd.to_datetime(df[col]).dt.date
+                elif target_type.startswith('datetime'):
                     if col == 'fecha' and table_name == 'carrefour_data':
                         df[col] = pd.to_datetime(df[col], format='%d/%m/%y')
                     else:
                         df[col] = pd.to_datetime(df[col])
                 elif target_type == 'string':
-                    target_type = 'str'
+                    df[col] = df[col].astype('str')
                 else:
                     df[col] = df[col].astype(target_type)
             except (ValueError, TypeError) as e:
                 print(f"⚠️ No se pudo convertir la columna {col} a {target_type}: {e}")
-                # Mantenemos el tipo original si falla la conversión
                 continue
         else:
-            # Intento de inferencia automática para columnas no mapeadas
+            # inferencia automática (igual que antes)
             try:
-                # Primero intentamos convertir a numérico
                 numeric_vals = pd.to_numeric(df[col], errors='raise')
-                # Si todos los valores son enteros, usamos int64, sino float64
                 if (numeric_vals % 1 == 0).all():
                     df[col] = numeric_vals.astype('int64')
                 else:
@@ -127,7 +122,6 @@ def convert_column_types(df, table_name):
                 pass
             
             try:
-                # Intentamos convertir a fecha/hora
                 datetime_vals = pd.to_datetime(df[col], errors='raise')
                 df[col] = datetime_vals
                 print(f"✅ Convertida columna {col} a datetime")
@@ -135,7 +129,6 @@ def convert_column_types(df, table_name):
             except (ValueError, TypeError):
                 pass
             
-            # Si no es numérico ni fecha, lo dejamos como string
             df[col] = df[col].astype('str')
     
     return df
@@ -205,6 +198,94 @@ def table_merge_staging_to_production_bq(bq_client, update_columns, target_table
         print(f"⚠️ Error inesperado: {str(e)}")
         return False
 
+def check_exists_and_prepare_schema_for_bq(client, tabla, staging_table_id):
+    df = convert_column_types(df, tabla)
+
+    for column, dtype in df.dtypes.items():
+        print(column, dtype)
+
+    table_has_data = False
+    table_exists = False
+    try:
+        table = client.get_table(staging_table_id)
+        table_exists = True
+        if table.num_rows > 0:
+            table_has_data = True
+            print(f"La tabla {staging_table_id} tiene {table.num_rows} filas.")
+        else:
+            table_has_data = False
+            print(f"La tabla {staging_table_id} está vacía.")
+    except NotFound:
+        table_exists = False
+
+    for col in ["INS_DTTM", "UPD_DTTM"]:
+        if col not in df.columns:
+            if col.lower() not in df.columns:
+                df[col] = pd.Timestamp.now(tz='UTC')
+            else:
+                # existe pero en miinuscula
+                df[col] = df[col].upper()
+
+    # Si no se provee schema, inferirlo del DataFrame
+    schema = build_bq_schema_from_df(df)
+    print('schema: ', schema)
+    
+    return df, schema, table_exists, table_has_data
+
+def build_bq_schema_from_df(df):
+    schema = []
+    for column, dtype in df.dtypes.items():
+        if pd.api.types.is_integer_dtype(dtype):
+            bq_type = "INTEGER"
+        elif pd.api.types.is_float_dtype(dtype):
+            bq_type = "FLOAT"
+        elif pd.api.types.is_bool_dtype(dtype):
+            bq_type = "BOOLEAN"
+        elif pd.api.types.is_datetime64_any_dtype(dtype):
+            bq_type = "TIMESTAMP"
+        elif dtype == "object" and df[column].apply(lambda x: isinstance(x, (datetime.date,))).all():
+            bq_type = "DATE"
+        else:
+            bq_type = "STRING"  # default
+        schema.append(bigquery.SchemaField(column, bq_type))
+
+    return schema
+
+def get_df_from_redshift_table(redshift_data, table_name):
+    response = redshift_data.execute_statement(
+        Database='dev',
+        WorkgroupName='pdf-etl-workgroup',
+        Sql= f"SELECT * FROM {table_name}"
+    )
+
+    query_id = response['Id']
+    while True:
+        status = redshift_data.describe_statement(Id=query_id)
+        if status['Status'] == 'FAILED':
+            print("❌ Query falló:", status['Error'])
+            break
+        elif status['Status'] == 'FINISHED':
+            if status.get('HasResultSet'):
+                results = redshift_data.get_statement_result(Id=query_id)
+                break
+            else:
+                print("⚠️ La query no devuelve resultados.")
+                break
+        time.sleep(1)
+
+    column_names = [col['name'] for col in results['ColumnMetadata']]
+    parsed_rows = []
+    for record in results['Records']:
+        parsed_row = []
+        for cell in record:
+            # Extrae el primer valor del diccionario (stringValue, longValue, etc.)
+            value = list(cell.values())[0]
+            parsed_row.append(value)
+        parsed_rows.append(dict(zip(column_names, parsed_row)))
+    df = pd.DataFrame(parsed_rows)
+
+    return df
+
 def upload_dataframe_to_bigquery(client, df, table_id, schema=None):    
     buffer = io.StringIO()
     df.to_csv(buffer, index=False)
@@ -239,129 +320,47 @@ def lambda_handler(event, context):
         
         stg_dataset_id = 'STG'
         stg_project_dataset = f'{project_id}.{stg_dataset_id}'
+        staging_table_id = f'{stg_project_dataset}.{tabla}'
 
         tbl_dataset_id = 'TBL'
         tbl_project_dataset = f'{project_id}.{tbl_dataset_id}'
+        prod_table_id = f'{tbl_project_dataset}.{tabla}'
 
         client = bigquery.Client(credentials=creds, project=f'{project_id}')
-
         redshift_data = boto3.client('redshift-data')
-        response = redshift_data.execute_statement(
-            Database='dev',
-            WorkgroupName='pdf-etl-workgroup',
-            Sql= f"SELECT * FROM {tabla}"
-        )
-
-        query_id = response['Id']
-        while True:
-            status = redshift_data.describe_statement(Id=query_id)
-            if status['Status'] == 'FAILED':
-                print("❌ Query falló:", status['Error'])
-                break
-            elif status['Status'] == 'FINISHED':
-                if status.get('HasResultSet'):
-                    results = redshift_data.get_statement_result(Id=query_id)
-                    break
-                else:
-                    print("⚠️ La query no devuelve resultados.")
-                    break
-            time.sleep(1)
-
-        column_names = [col['name'] for col in results['ColumnMetadata']]
-        parsed_rows = []
-        for record in results['Records']:
-            parsed_row = []
-            for cell in record:
-                # Extrae el primer valor del diccionario (stringValue, longValue, etc.)
-                value = list(cell.values())[0]
-                parsed_row.append(value)
-            parsed_rows.append(dict(zip(column_names, parsed_row)))
-        df = pd.DataFrame(parsed_rows)
-        df = convert_column_types(df, tabla)
-
-        for column, dtype in df.dtypes.items():
-            print(column, dtype)
-
-        # Primero vemos si existe la tabla
-        staging_table_id = f'{stg_project_dataset}.{tabla}'
-        try:
-            client.get_table(staging_table_id)
-            table_exists = True
-        except NotFound:
-            table_exists = False
+        
+        df = get_df_from_redshift_table(redshift_data, tabla)
+        
+        ################# TABLA  STAGING ######################
+        df, schema, table_exists, table_has_data = check_exists_and_prepare_schema_for_bq(client, tabla, staging_table_id)        
 
         # Si no existe la tabla, la creamos de forma dinamica con las columnas del dataframe y luego transferimos los datos de redshift a bigquery a traves de pandas df
         if not table_exists:
             print(f"🆕 La tabla en staging {staging_table_id} no existe. Creándola...")
-            
-            # Manejar advertencia de pandas-gbq
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=FutureWarning)
-                
-                for col in ["INS_DTTM", "UPD_DTTM"]:
-                    if col not in df.columns:
-                        if col.lower() not in df.columns:
-                            df[col] = pd.Timestamp.now(tz='UTC')
-                        else:
-                            # existe pero en miinuscula
-                            df[col] = df[col].upper()
-
-                # Si no se provee schema, inferirlo del DataFrame
-                schema = []
-                for column, dtype in df.dtypes.items():
-                    # Mapear tipos de pandas a BigQuery
-                    if pd.api.types.is_integer_dtype(dtype):
-                        bq_type = "INTEGER"
-                    elif pd.api.types.is_float_dtype(dtype):
-                        bq_type = "FLOAT"
-                    elif pd.api.types.is_bool_dtype(dtype):
-                        bq_type = "BOOLEAN"
-                    elif pd.api.types.is_datetime64_any_dtype(dtype):
-                        bq_type = "TIMESTAMP"
-                    else:
-                        bq_type = "STRING"  # Default para otros tipos
-                    
-                    schema.append(bigquery.SchemaField(column, bq_type))
-                        
-            # job_config = bigquery.LoadJobConfig(
-            #     schema = schema,
-            #     write_disposition="WRITE_EMPTY",
-            #     autodetect=False
-            # )
-
-            # job = client.load_table_from_dataframe(df, staging_table_id, job_config=job_config)
-            # job.result()
-
             upload_dataframe_to_bigquery(client, df, staging_table_id, schema)
-
-            # df.to_gbq(
-            #     destination_table=f"{stg_dataset_id}.{tabla}", 
-            #     project_id=project_id,
-            #     if_exists="replace"
-            # )
-
-            print("✅ Tabla creada y datos cargados.")
-
-        # Si la tabla ya existe, solo transferimos los datos de redshift a bigquery a traves de pandas df
+        # Si la tabla ya existe, no deberia tener datos porque siempre hacemos un TRUNC TABLE despues de mergear la tabla de staging con la productiva
+        # Procedemos a cargarle los datos del dataframe
         else:
-            # Creo que deberia hacer un merge aca?
-            # job_config = bigquery.LoadJobConfig(
-            #     write_disposition="WRITE_TRUNCATE",
-            #     autodetect=True
-            # )
-
-            # job = client.load_table_from_dataframe(df, staging_table_id, job_config=job_config)
-            # job.result()
-
+            print("📊 La tabla existe pero no tiene datos, ejecutamos insert")
             upload_dataframe_to_bigquery(client, df, staging_table_id, schema)
+        print("✅ Datos cargados")
 
-            # df.to_gbq(
-            #     destination_table=f"{stg_dataset_id}.{tabla}", 
-            #     project_id=project_id,
-            #     if_exists="replace"
-            # )
-            print("✅ Tabla ya existe, datos cargados.")
-    
+        ################# TABLA  PRODUCTIVA ######################
+        df, schema, table_exists = check_exists_and_prepare_schema_for_bq(client, tabla, prod_table_id)    
+
+        # Crear tabla productiva si no existe
+        if not table_exists:
+            print(f"🆕 La tabla en prod {prod_table_id} no existe. Creándola...")
+            upload_dataframe_to_bigquery(client, df, prod_table_id, schema)
+            print(f"🆕 La tabla en prod {prod_table_id} no existe. Creándola...")
+
+        # Si la tabla ya existe, aunque ya tuviera datos, le insertamos los datos de nuevo, luego hacemos un merge y se evitan los duplicados
+        # y tambien se trunca la tabla de staging. Aca solo transferimos los datos de redshift a bigquery a traves de pandas df.
+        else:
+            upload_dataframe_to_bigquery(client, df, prod_table_id, schema)
+            print(f"✅ Tabla productiva {prod_table_id} creada.")
+
+        ################# MERGE STAGING => PROD ######################
         # Hacemos el merge de la tabla de staging de BQ a la tabla productiva de BQ
         if tabla == 'mp_data':
             pk = 'report_id'
@@ -375,59 +374,6 @@ def lambda_handler(event, context):
             pk = 'id'
 
         update_columns = [col for col in df.columns if col not in [pk, 'INS_DTTM', 'UPD_DTTM']]
-
-        # Crear tabla productiva si no existe
-        prod_table_id = f'{tbl_project_dataset}.{tabla}'
-        try:
-            client.get_table(prod_table_id)
-            prod_table_exists = True
-        except NotFound:
-            prod_table_exists = False
-
-        if not prod_table_exists:
-            print(f"🆕 La tabla en produccion {prod_table_id} no existe. Creándola...")
-
-            for col in ["INS_DTTM", "UPD_DTTM"]:
-                if col not in df.columns:
-                    if col.lower() not in df.columns:
-                        df[col] = pd.Timestamp.now(tz='UTC')
-                    else:
-                        # existe pero en miinuscula
-                        df[col] = df[col].upper()
-
-            # Usar el mismo esquema que la tabla de staging
-            schema = []
-            for column, dtype in df.dtypes.items():
-                if pd.api.types.is_integer_dtype(dtype):
-                    bq_type = "INTEGER"
-                elif pd.api.types.is_float_dtype(dtype):
-                    bq_type = "FLOAT"
-                elif pd.api.types.is_bool_dtype(dtype):
-                    bq_type = "BOOLEAN"
-                elif pd.api.types.is_datetime64_any_dtype(dtype):
-                    bq_type = "TIMESTAMP"
-                else:
-                    bq_type = "STRING"
-                schema.append(bigquery.SchemaField(column, bq_type))
-
-            # job_config = bigquery.LoadJobConfig(
-            #     schema=schema,
-            #     write_disposition="WRITE_EMPTY",
-            #     autodetect=False
-            # )
-            # job = client.load_table_from_dataframe(df, prod_table_id, job_config=job_config)
-            # job.result()
-
-            upload_dataframe_to_bigquery(client, df, prod_table_id, schema)
-
-            # df.to_gbq(
-            #     destination_table=f"{tbl_dataset_id}.{tabla}", 
-            #     project_id=project_id,
-            #     if_exists="replace"
-            # )
-
-            print(f"✅ Tabla productiva {prod_table_id} creada.")
-
         result = table_merge_staging_to_production_bq(client, update_columns, tabla, staging_table_id, pk, tbl_project_dataset)
 
         # Si el merge se ejecutó bien,
