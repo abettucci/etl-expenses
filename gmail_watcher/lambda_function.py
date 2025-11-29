25220d26c3ec82e07f7f20f41ab6c3067f3fc046
abettucci <abettucci@users.noreply.github.com>
2025-11-29 20:08:32 -0300

    fix en watcher

--- gmail_watcher/lambda_function.py
+++ gmail_watcher/lambda_function.py
@@ -33,24 +33,29 @@ def auth_google(SECRET_NAME):
     return creds
 
 def lambda_handler(event, context):
-    # creds = auth_google('gcp_sa_api_credentials')
     creds = auth_google('gcp_sa_api_credentials')
     gmail_service = build('gmail', 'v1', credentials=creds)
+    
     results = gmail_service.users().labels().list(userId="me").execute()
-    label_ids = []
+    
+    custom_labels = []
+    inbox_label_id = None
 
     for label in results['labels']:
+        if label['name'] == 'INBOX':
+            inbox_label_id = label['id']
         if label['name'] in ['Avisos Gastos Santander', 'Avisos Compra Carrefour']:
-            label_ids.append(label['id'])
+            custom_labels.append(label['id'])
 
-    if label_ids:
-        body = {
-            "labelIds": label_ids,
-            "topicName": f"projects/{os.environ['GCP_PROJECT_ID']}/topics/{os.environ['PUBSUB_TOPIC']}"
-        }
-        resp = gmail_service.users().watch(userId="me", body=body).execute()
+    if inbox_label_id is None:
+        raise Exception("❌ No encontré el label INBOX (esto no debería pasar).")
 
-        print("Watcher renewed:", resp)
-        return {"status": "ok", "response": resp}
-    else:
-        print("❌ No encontré las etiquetas configuradas")
+    body = {
+        "labelIds": [inbox_label_id] + custom_labels,
+        "topicName": f"projects/{os.environ['GCP_PROJECT_ID']}/topics/{os.environ['PUBSUB_TOPIC']}"
+    }
+
+    resp = gmail_service.users().watch(userId="me", body=body).execute()
+
+    print("Watcher renewed:", resp)
+    return {"status": "ok", "response": resp}
